644b6bf81d12973636612db7b8f99701a16a926b
mgarcia <mateus.gigainfo@gmail.com>
2018-02-01 18:52:36 -0200

    Nova procedure para inserir em ListaDeVagas

--- BancoDeDados/CREATE.sql
+++ BancoDeDados/CREATE.sql
@@ -101,7 +101,23 @@ ON UPDATE CASCADE
 */
 DELIMITER $
 CREATE PROCEDURE InsereProfissaoDoCandidato (IN IdCand INT, IN prof VARCHAR(45))
+BEGIN
 INSERT INTO CandidatoXProfissao (IdCandidato, IdProfissao) VALUES
+(
+(IdCand),
+(SELECT IdProfissao FROM Profissao WHERE Nome = prof)
+);
+END$
+DELIMITER ;
+
+/*
+Esta procedure agiliza a inserção de Concursos Publicos e suas profissões oferecidas
+*/
+
+DELIMITER $
+CREATE PROCEDURE InsereProfissaoDoConcurso (IN IdConc INT, IN prof VARCHAR(45))
+BEGIN
+INSERT INTO ListaDeVagas (IdConcursoPublico, IdProfissao) VALUES
 (
 (IdCand),
 (SELECT IdProfissao FROM Profissao WHERE Nome = prof)
--- BancoDeDados/PROCEDURES.sql
+++ BancoDeDados/PROCEDURES.sql
@@ -1,12 +1,30 @@
+/*
+Esta procedure agiliza a inserção de Candidatos e suas profissões selecionadas
+*/
+
 DELIMITER $
-
 CREATE PROCEDURE InsereProfissaoDoCandidato (IN IdCand INT, IN prof VARCHAR(45))
-
+BEGIN
 INSERT INTO CandidatoXProfissao (IdCandidato, IdProfissao) VALUES
 (
-	(IdCand),
-	(SELECT IdProfissao FROM Profissao WHERE Nome = prof)
+(IdCand),
+(SELECT IdProfissao FROM Profissao WHERE Nome = prof)
 );
-END $
+END$
+DELIMITER ;
 
+
+/*
+Esta procedure agiliza a inserção de Concursos Publicos e suas profissões oferecidas
+*/
+
+DELIMITER $
+CREATE PROCEDURE InsereProfissaoDoConcurso (IN IdConc INT, IN prof VARCHAR(45))
+BEGIN
+INSERT INTO ListaDeVagas (IdConcursoPublico, IdProfissao) VALUES
+(
+(IdCand),
+(SELECT IdProfissao FROM Profissao WHERE Nome = prof)
+);
+END$
 DELIMITER ;
