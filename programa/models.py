db08067eca5a756d8ad10b6837db247e21c06c7e
lukasg18 <lukas.gomes2010@gmail.com>
2018-02-01 00:01:17 -0200

    finalizando models

--- programa/models.py
+++ programa/models.py
@@ -5,8 +5,6 @@ from django.utils import timezone
 class Profissao(models.Model):
     nome = models.CharField(max_length=50)
 
-    def __str__(self):
-        return self.nome
 
 class Candidato(models.Model):
     nome = models.CharField(max_length=100)
@@ -14,23 +12,14 @@ class Candidato(models.Model):
     cpf = models.CharField(max_length=14)
     profissao = models.ForeignKey(Profissao)
 
-    def __str__(self):
-        return self.nome
 
 class Orgao(models.Model):
     nome = models.CharField(max_length=10)
 
-    def publish(self):
-        self.save()
 
-    def __str__(self):
-        return self.nome
 
 class Concurso(models.Model):
     orgao = models.ForeignKey(Orgao)
     edital = models.CharField(max_length=10)
     codigo_curso = models.CharField(max_length=14)
     vagas = models.ForeignKey(Profissao)
-
-    def __str__(self):
-        return self.edital
