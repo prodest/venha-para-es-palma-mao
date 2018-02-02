b3dad772ec61b6917c4a2979b48943e5a635cac1
mgarcia <mateus.gigainfo@gmail.com>
2018-02-02 00:07:21 -0200

    Querys solucionadas

--- BancoDeDados/QUERYS.sql
+++ BancoDeDados/QUERYS.sql
@@ -1,7 +1,36 @@
 
 /* Esta query mostra as profissoes que o candidato selecionou */
 SELECT Profissao.IdProfissao FROM Profissao JOIN CandidatoXProfissao ON Profissao.IdProfissao = CandidatoXProfissao.IdProfissao
-JOIN Candidato ON CandidatoXProfissao.IdCandidato = Candidato.IdCandidato WHERE Candidato.CPF = '120.164.449-15';
+JOIN Candidato ON CandidatoXProfissao.IdCandidato = Candidato.IdCandidato WHERE Candidato.CPF = '551.235.392-12';
 
 /* Esta query mostra os concursos disponiveis por profissao */
+SELECT ConcursoPublico.IdConcursoPublico FROM ConcursoPublico JOIN ListaDeVagas ON
+ListaDeVagas.IdConcursoPublico = ConcursoPublico.IdConcursoPublico WHERE ListaDeVagas.IdProfissao IN (SELECT Profissao.IdProfissao FROM Profissao JOIN CandidatoXProfissao ON Profissao.IdProfissao = CandidatoXProfissao.IdProfissao
+JOIN Candidato ON CandidatoXProfissao.IdCandidato = Candidato.IdCandidato WHERE Candidato.CPF = '551.235.392-12');
+
+##################################################################################################################################
+SOLUÇÃO DO PROBLEMA 1
+/* Listar Orgaos, Codigos e editais pelo CPF */
+SELECT Orgao, CodConcurso, EditalNum, EditalAno FROM ConcursoPublico WHERE IdConcursoPublico IN
+(SELECT ConcursoPublico.IdConcursoPublico FROM ConcursoPublico JOIN ListaDeVagas ON
+ListaDeVagas.IdConcursoPublico = ConcursoPublico.IdConcursoPublico WHERE ListaDeVagas.IdProfissao IN (SELECT Profissao.IdProfissao FROM Profissao JOIN CandidatoXProfissao ON Profissao.IdProfissao = CandidatoXProfissao.IdProfissao
+JOIN Candidato ON CandidatoXProfissao.IdCandidato = Candidato.IdCandidato WHERE Candidato.CPF = '120.164.449-15'));
+##################################################################################################################################
+SOLUÇÃO DO PROBLEMA 2
+SELECT Nome, DataNasc, CPF FROM Candidato JOIN CandidatoXProfissao ON Candidato.IdCandidato = CandidatoXProfissao.IdCandidato
+JOIN ListaDeVagas ON CandidatoXProfissao.IdProfissao = ListaDeVagas.IdProfissao 
+JOIN ConcursoPublico ON ListaDeVagas.IdConcursoPublico = ConcursoPublico.IdConcursoPublico WHERE ConcursoPublico.CodConcurso = '80671076246';
+##################################################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
 
