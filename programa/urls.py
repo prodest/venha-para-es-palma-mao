a4671cedab638a8d81325f608ea5a1324786e427
lukasg18 <lukas.gomes2010@gmail.com>
2018-02-02 15:28:04 -0200

    pagina de detalhes do candidado

--- programa/urls.py
+++ programa/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.candidato_list, name='candidato_list'),
+    url(r'^cand/(?P<pk>[0-9]+)/$', views.candidato_detail, name='candidato_detail'),
 ]
--- programa/views.py
+++ programa/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
 # Create your views here.
 
 def candidato_list(request):
     candidatos = Candidato.objects.all()
     return render(request, 'programa/candidato_list.html', {'candidatos':candidatos})
+
+def candidato_detail(request, pk):
+    candidatos = get_object_or_404(Candidato, pk=pk)
+    return render(request, 'programa/candidato_detail.html', {'candidatos':candidatos})
 
