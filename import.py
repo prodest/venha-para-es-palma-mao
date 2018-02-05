8401b6a6fdd148983433d22d70dd60280454aeb1
lukasg18 <lukas.gomes2010@gmail.com>
2018-02-05 19:46:44 -0200

    acertando erro de objeto ao salvar

--- import.py
+++ import.py
@@ -11,8 +11,19 @@ import django
 
 django.setup()
 
+#importando os modelos a serem usados
+from programa.models import *
+
+#declaraçao de obejtos
+candidato = Candidato()
+orgao = Orgao()
+profissao = Profissao()
+concurso = Concurso()
+
 #abrindo o arquivo
 arq = open('candidatos.txt', 'r')
+texto = arq.readline()
+texto = arq.readline()
 texto = arq.readline()
 
 #caracteres a serem removidos da linha
@@ -20,11 +31,34 @@ remove = "[]\n"
 for i in range(0, len(remove)) :
     texto = texto.replace(remove[i],"")
 
-#cortando o texto em partes
-print(texto)
+#após a limpeza, corto o texto em partes
+# print(texto)
 informacoes = texto.split(" ", 4)
-print(val)
-profissao = val[4].split(",")
-print(test)
-print(val[0]+ " " + val[1])
+# print(informacoes)
+list_prof = informacoes[4].split(",")
+# print(list_prof)
+# print(informacoes[0]+ " " + informacoes[1])
+
+
+for prof in list_prof :
+    candidato = Candidato()
+    profissao = Profissao()
+    profissao.nome = prof
+    print(profissao.nome)
+    candidato.nome = informacoes[0]+ " " + informacoes[1]
+    candidato.data_nascimento = informacoes[2]
+    candidato.cpf = informacoes[3]
+    if Profissao.objects.filter(nome=profissao.nome).exists():
+        print("eu existo!")
+        profissao = Profissao.objects.get(nome=profissao.nome)
+        candidato.profissao = profissao
+        candidato.save()
+    else:
+        print("eu existo!")
+        profissao.save()
+        profissao = Profissao.objects.get(nome=profissao.nome)
+        candidato.profissao = profissao
+        candidato.save()
+
+print(" OK ")
 arq.close()
