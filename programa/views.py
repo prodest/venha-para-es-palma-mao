3e527a72e6a8f9e66900427542fb272629aaca03
lukasg18 <lukas.gomes2010@gmail.com>
2018-02-07 19:01:51 -0200

    implementando search box

--- programa/views.py
+++ programa/views.py
@@ -3,8 +3,16 @@ from .models import *
 # Create your views here.
 
 def candidato_list(request):
-    candidatos = Candidato.objects.all()
-    return render(request, 'programa/candidato_list.html', {'candidatos':candidatos})
+    var_get_search = request.GET.get('search_box')
+    if var_get_search is not None:
+        candidatos = Candidato.objects.all()
+        verifica = False
+        verifica = True
+        candidatos = candidatos.filter(cpf=var_get_search)
+        return render(request, 'programa/candidato_list.html', {'candidatos':candidatos})
+    return render(request, 'programa/candidato_list.html', {})
+    # candidatos = Candidato.objects.all()
+    # return render(request, 'programa/candidato_list.html', {'candidatos':candidatos})
 
 def candidato_detail(request, pk):
     candidatos = get_object_or_404(Candidato, pk=pk)
