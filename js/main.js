ffaeec57cea2859a5d8a06c52398bb02ce760af4
magnoDev <magno_macedo2099@hotmail.com>
2018-02-07 00:56:13 -0200

    Inicio do projeto. Criação dos arquivos front-end. Preparação da tela inicial.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/* Funcao que verifica a opcao marcada e altera o texto do placeholder
+pertencente ao input search (txtPesquisa) */
 function exibeMsg(valor){
     if (valor == 'candidato') {
         document.getElementById('txtPesquisa').placeholder = "Informe o CPF do candidato..."
