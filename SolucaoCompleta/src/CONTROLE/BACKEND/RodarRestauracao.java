50aff3160bdf7e5da0d3ebe8f08457a5e121682d
mgarcia <mateus.gigainfo@gmail.com>
2018-02-07 00:38:58 -0200

    UNIFICAÇÃO DAS SOLUÇÕES E SIMPLIFICAÇÃO DO FLUXO

---
+++ SolucaoCompleta/src/CONTROLE/BACKEND/RodarRestauracao.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2018 mgarcia
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package CONTROLE.BACKEND;
+
+import CONTROLE.DAO.CandidatoDAO;
+import CONTROLE.DAO.ConcursoPublicoDAO;
+import CONTROLE.DAO.VerificaStatusDAO;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author mgarcia
+ */
+
+/*
+    *  Esta classe le os dados contidos nos arquivos, formatando-os nos seus
+    *  respectivos formatos e os salvando no banco de dados respeitando a 
+    *  integridade dos dados atraves dos autocommits desabilitados. cada entidade
+    *  e suas profissoes só tem seus inserts commitados ao final de cada iteração
+    *  finalizada sem erros.
+ */
+public class RodarRestauracao {
+
+    // o construtor recebe os Paths para os arquivos já tratados
+    public RodarRestauracao(String[] arquivos) throws Exception {
+        //faz a verificação se o banco existe e se so arquivos ja foram processados antes
+        //caso as condições estejam como esperado, executa a restauração
+        if (VerificaStatusDAO.BancoExiste()) {
+            if (!VerificaStatusDAO.ArquivosProcessados()) {
+
+                String candidatostxt = arquivos[0];
+                String concursostxt = arquivos[1];
+
+                // esta é uma lista de todas as linhas que serão lidas do arquivo
+                ArrayList<String> candidatos;
+
+                try {
+                    JOptionPane.showMessageDialog(null, "Os arquivos serão processados AGORA!");
+                    System.out.println("Aguarde, lendo o arquivo candidatos.txt...");
+                    //instanciando uma lista de candidatos lida do arquivo
+                    candidatos = LeitorDeArquivo.LeArquivoCandidatos(candidatostxt);
+
+                    CandidatoDAO candao = new CandidatoDAO();
+                    System.out.println("Iniciando as inserções no banco de dados...");
+                    try {
+                        //entra num loop e vai salvando os candidatos da lista 1 por 1
+                        for (int i = 0; i < candidatos.size(); i++) {
+                            candao.salvar(LeitorDeDados.LeCandidatos(candidatos.get(i)));
+                        }
+                        System.out.println("Inserções Concluídas com Êxito!");
+                    } catch (Exception ex) {
+                        Logger.getLogger(RodarRestauracao.class.getName()).log(Level.SEVERE, null, ex);
+                        System.out.println("Erro ao tentar salvar Candidatos\n" + ex);
+                    }
+
+                } catch (IOException ex) {
+                    Logger.getLogger(RodarRestauracao.class.getName()).log(Level.SEVERE, null, ex);
+                    System.out.println("Erro ao ler o arquivo\n" + ex);
+                }
+
+                ArrayList<String> concursos; // lista de linhas lidas do arquivo
+                System.out.println("Aguarde, lendo o arquivo concursos.txt...");
+
+                try {
+                    concursos = LeitorDeArquivo.LeArquivoCandidatos(concursostxt);
+                    ConcursoPublicoDAO codao = new ConcursoPublicoDAO();
+                    System.out.println("Iniciando as inserções no banco de dados...");
+                    try {
+                        //entra num loop e vai salvando os Concursos da lista 1 por 1
+                        for (int i = 0; i < concursos.size(); i++) {
+                            codao.salvar(LeitorDeDados.LeConcursos(concursos.get(i)));
+                        }
+                        System.out.println("Inserções Concluídas com Êxito!");
+                    } catch (Exception ex) {
+                        Logger.getLogger(RodarRestauracao.class.getName()).log(Level.SEVERE, null, ex);
+                        System.out.println("Erro ao tentar salvar Concursos\n" + ex);
+                    }
+
+                } catch (IOException ex) {
+                    Logger.getLogger(RodarRestauracao.class.getName()).log(Level.SEVERE, null, ex);
+                    System.out.println("Erro ao ler o arquivo\n" + ex);
+                }
+
+            } else {
+                JOptionPane.showMessageDialog(null, "Os arquivos ja foram processados"
+                        + "antes e não precisam ser processados novamente");
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Banco de dados invalido!");
+        }
+    }
+
+}
---
+++ SolucaoCompleta/src/VISAO/AppStart.java
@@ -0,0 +1,172 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VISAO;
+
+import CONTROLE.DAO.VerificaStatusDAO;
+import static CONTROLE.DAO.VerificaStatusDAO.ResetConfigs;
+import static CONTROLE.DAO.VerificaStatusDAO.TheConfIsValid;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author mgarcia
+ */
+public class AppStart {
+
+    //este boolean sera manipulado pela janela de configuração do .conf
+    public static String candidatostxt;
+    public static String concursostxt;
+    //paths para os arquivos ^
+
+    //padrão do NetBeans levemente customizado para facilitar a leitura
+    //este método foi retirado do main padrão dos JFrames que o NetBeans gera
+    //sua serventia é deixar o visual dos JFrames mais bonitos.
+    public static void setNimbusVisual() {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(ConfiguraConf.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(ConfiguraConf.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(ConfiguraConf.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(ConfiguraConf.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+    }
+
+    //a mágica começa aqui
+    public static void main(String[] args) {
+        //inicia com aquele padrão do NetBeans enxugado
+        setNimbusVisual();
+
+        /*
+        *   ************************* PASSO 1 *******************************
+        *   VOCÊ DEVE CONFIGURAR O SERVIDOR MYSQL COM O SCRIPT CREATE.sql
+        *   QUE ACOMPANHA ESTE PROJETO NA PASTA BancoDeDados.
+        *   AO PREPARAR O SERVIDOR, ANOTE O USUARIO, SENHA IP E PORTA,
+        *   O PROGRAMA SOLICITARÁ AS INFORMAÇÕES CASO NÃO ESTEJAM CONFIGURADAS
+        *   NO ARQUIVO mysql.conf QUE ESTÁ NA PASTA RAIZ DO PROJETO
+        *   *****************************************************************
+         */
+        if (false == TheConfIsValid()) {
+            JOptionPane.showMessageDialog(null, "Antes de rodar a aplicação, crie o "
+                    + "Banco de dados em um servidor MySQL \nusando o script "
+                    + "CREATE.sql que acompanha este Projeto na pasta "
+                    + "'BancoDeDados'\nAnote a porta do servidor, o usuario e a "
+                    + "senha. A aplicação irá solicitar esses dados a seguir");
+
+            /*
+        *   ************************ PASSO 2 ********************************
+        *   APÓS TER CONFIGURADO O SERVIDOR MYSQL COM O SCRIPT CREATE.sql
+        *   QUE ACOMPANHA O PROJETO NA PASTA BancoDeDados, A APLICAÇÃO IRÁ
+        *   VERIFICAR SE O ARQUIVO DE CONFIGURAÇÃO DO BANCO, O mysql.conf,
+        *   QUE ESTÁ NA PASTA RAIZ DO PROJETO, ESTÁ DEVIDAMENTE CONFIGURADO.
+        *   NA PRIMEIRA EXECUÇÃO ELE NÃO ESTARÁ, ENTÃO VOCÊ DEVERÁ INFORMAR
+        *   OS DADOS ANOTADOS NO PASSO 1 AO FORMULARIO QUE SERÁ EXIBIDO.
+        *   A APLICAÇÃO SE ENCARREGA DE CONFIGURAR O ARQUIVO SEM INTERVENÇÃO
+        *   MANUAL NO MESMO.
+        *   *****************************************************************
+             */
+            if (false == TheConfIsValid()) {
+                JOptionPane.showMessageDialog(null, "Parece que seu arquivo de "
+                        + "configuração do banco de dados ainda não está bem "
+                        + "configurado. Vamos resolver isso");
+                new ConfiguraConf(null, true).setVisible(true);
+            }
+        }
+
+        /*
+            *   *************************** PASSO 3 *************************
+            *   SE O FLUXO CHEGAR NESTE PONTO, O ARQUIVO mysql.conf ESTÁ
+            *   DEVIDAMENTE CONFIGURADO(espero que sim :-)). AGORA A 
+            *   APLICAÇÃO IRÁ VERIFICAR A CONEXÃO COM O BANCO DE DADOS E 
+            *   CHECAR SE OS ARQUIVOS JÁ FORAM PROCESSADOS ANTERIORMENTE.
+            *   CASO A LÓGICA CONCLUA QUE O BANCO JÁ EXISTE, QUE O SCHEMA ESTÁ 
+            *   EM ORDEM E QUE OS ARQUIVOS JÁ FORAM PROCESSADOS, ESTA ETAPA
+            *   SERÁ SUMARIAMENTE IGNORADA E A APLICAÇÃO IRÁ ABRIR A TELA
+            *   PRINCIPAL, ONDE A MISSÃO SERÁ CUMPRIDA COM O ÊXITO ESPERADO.
+            *   CASO CONTRARIO, A APLICAÇÃO IRÁ TENTAR CONFIGURAR O AMBIENTE
+            *   COM AUXILIO DO OPERADOR DO SISTEMA OU USUÁRIO.
+            *   *************************************************************
+         */
+        // LÓGICA PARA VERIFICAR O AMBIENTE
+        boolean bancoexiste = VerificaStatusDAO.BancoExiste();
+        boolean arquivosprocessados = false;
+        if (TheConfIsValid()) {
+            if (!bancoexiste) {
+                JOptionPane.showMessageDialog(null, "O Banco de Dados não foi "
+                        + "encontrado! Execute o script CREATE.sql no "
+                        + "Servidor do  MySQL e certifique-se \nque seu usuario"
+                        + " possui as permissões adequadas para criar schemas"
+                        + " e tabelas.\nVerifique o arquivo de configuração "
+                        + "mysql.conf na pasta raiz da aplicação");
+                ResetConfigs(); //reseta o mysql.conf
+                System.exit(0);
+            }
+            if (bancoexiste) {
+                //o banco existe e está configurado
+                try {
+                    arquivosprocessados = VerificaStatusDAO.ArquivosProcessados();
+                    // agora x informa se os arquivos estão processados ou nao
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null, "Erro ao checar os dados" + e);
+                    System.exit(0);
+                }
+            }
+
+            /*
+            * *************************** PASSO 4 *************************
+            *   NESTA ETAPA, APÓS CONFIRMAR QUE O BANCO EXISTE E ESTÁ 
+            *   CONFIGURADO COM O SCHEMA Concursos E POSSUI AS 5 TABELAS,
+            *   E APÓS VERIFICAR QUE OS ARQUIVOS AINDA NÃO FORAM PROCESSADOS,
+            *   O PROGRAMA IRÁ ABRIR O SELETOR DE ARQUIVOS ONDE O OPERADOR
+            *   IRÁ APONTAR PARA OS ARQUIVOS candidatos.txt E concursos.txt
+            *   APÓS APONTAR PARA OS ARQUIVOS, O SELETOR IRÁ DIRECIONAR O
+            *   FLUXO PARA OS DEVIDOS BACKENDS, ONDE A MÁGICA ACONTECERÁ.
+            *   ************************************************************
+             */
+            if (bancoexiste && !arquivosprocessados) {
+                //os arquivos não estão processados. processe!
+                new SeletorDeArquivos(null, true).setVisible(true);
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "O arquivo de configuração "
+                    + "mysql.inf é invalido");
+            System.exit(0);
+        }
+
+        /*
+        *   *************************** PASSO 5 *************************
+        *   APÓS O BANCO ESTAR ONLINE, CONFIGURADO, COM OS DADOS DOS
+        *   ARQUIVOS JÁ PROCESSADOS E ARMAZENADOS LA DENTRO, O PROGRAMA
+        *   ENTRARÁ EM SEU PRINCIPAL ESTÁGIO: A TELA PRINCIPAL SERÁ 
+        *   EXIBIDA E O OPERADOR PODERÁ FAZER AS DUAS CONSULTAS QUE
+        *   O TESTE DA PRODEST SOLICITOU! THAT'S IT! GG WP!
+        *   ************************************************************
+         */
+        
+        
+        if (bancoexiste && arquivosprocessados) {
+            new AppMainStart().setVisible(true);
+        }
+        
+        //PRONTO!
+        //MATEUS GARCIA LOPES
+        //INSCRIÇÃO 750838
+    }
+}
