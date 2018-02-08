bba8cef137a2e219edf578746783ff721f2f6ba9
lukasg18 <lukas.gomes2010@gmail.com>
2018-02-08 16:30:02 -0200

    index.html criado

--- programa/urls.py
+++ programa/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^candidato/$', views.candidato_list, name='candidato_list'),
+    url(r'^concurso/$', views.concurso_list, name='concurso_list'),
     url(r'^cand/(?P<pk>[0-9]+)/$', views.candidato_detail, name='candidato_detail'),
 ]
--- programa/views.py
+++ programa/views.py
@@ -19,6 +19,20 @@ def candidato_list(request):
 
     return render(request, 'programa/candidato_list.html', {})
 
+def concurso_list(request):
+    candidatos = Candidato()
+    concursos = Concurso()
+    var_get_search = request.GET.get('search_box')
+
+    if var_get_search is not None:
+        concursos = Concurso.objects.all()
+        concursos = concursos.filter(codigo_curso=var_get_search)
+        for elem in concursos : 
+            candidatos = Candidato.objects.filter(profissao=elem.vagas)
+        return render(request, 'programa/concurso_list.html', {'candidatos':candidatos})
+
+    return render(request, 'programa/concurso_list.html', {})
+
 def candidato_detail(request, pk):
     candidatos = get_object_or_404(Candidato, pk=pk)
     return render(request, 'programa/candidato_detail.html', {'candidatos':candidatos})
