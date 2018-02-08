faa01e0debc0863abf0d031d8d4d26ee3241e77c
lukasg18 <lukas.gomes2010@gmail.com>
2018-02-08 15:27:08 -0200

    search bar implementado

--- programa/urls.py
+++ programa/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.candidato_list, name='candidato_list'),
+    url(r'^$', views.home, name='home'),
+    url(r'^candidato/$', views.candidato_list, name='candidato_list'),
     url(r'^cand/(?P<pk>[0-9]+)/$', views.candidato_detail, name='candidato_detail'),
 ]
--- programa/views.py
+++ programa/views.py
@@ -2,20 +2,25 @@ from django.shortcuts import render, get_object_or_404
 from .models import *
 # Create your views here.
 
+def home(request):
+    return render(request, 'programa/index.html', {} )
+
 def candidato_list(request):
+    candidatos = Candidato()
+    concursos = Concurso()
     var_get_search = request.GET.get('search_box')
+
     if var_get_search is not None:
         candidatos = Candidato.objects.all()
-        verifica = False
-        verifica = True
         candidatos = candidatos.filter(cpf=var_get_search)
-        return render(request, 'programa/candidato_list.html', {'candidatos':candidatos})
+        for elem in candidatos : 
+            concursos = Concurso.objects.filter(vagas=elem.profissao)
+        return render(request, 'programa/candidato_list.html', {'concursos':concursos})
+
     return render(request, 'programa/candidato_list.html', {})
-    #apagar isto
-    # candidatos = Candidato.objects.all()
-    # return render(request, 'programa/candidato_list.html', {'candidatos':candidatos})
 
 def candidato_detail(request, pk):
     candidatos = get_object_or_404(Candidato, pk=pk)
     return render(request, 'programa/candidato_detail.html', {'candidatos':candidatos})
+
 
