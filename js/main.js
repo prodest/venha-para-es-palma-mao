f4bf0946ef78e9fc0469b5b2ceceed5cc1663ef5
magnoDev <magno_macedo2099@hotmail.com>
2018-02-13 00:02:00 -0200

    Realizando alterações gerais e criando os tratamentos com jQuery

--- js/main.js
+++ js/main.js
@@ -1,12 +1,40 @@
-$(function() {
+$(document).ready(function() {
 
+    $("#divCarregando").hide();
+
+    /**
+     * Altera de forma dinâmica o placeholder do campo de texto de acordo com o
+     * que for marcado dentre as opções 'candidato' ou 'concurso' e aplicar a
+     * máscara para o cpf, caso seja candidato, e personalizada para o código do concurso.
+     */
     $("input[name='escolha']").change(function(){
         $("#txtPesquisa").attr("placeholder", $("input[name='escolha']:checked").attr("data-placeholder"));
+        
+        var escolha = $("input[name='escolha']:checked").val();
+        
+        if (escolha == "candidato") {
+            $("#txtPesquisa").mask('000.000.000.-00');
+        }else if (escolha == "concurso") {
+            $("#txtPesquisa").mask('x9999999999',  {
+                                                        translation: {
+                                                            'x': {
+                                                                pattern: /[^(0+)]/
+                                                            }
+                                                        }
+                                                    });
+        }
     });
 
+    /**
+     * Faz o carregamento dos arquivos JSON e retorna uma tabela com os valores
+     * de acordo com o que for passado pelo usuário.
+     */
     $("#btnBusca").click(function() {
 
         $("#tabela").empty();
+
+        $("#divCarregando").show();
+        $('#divCarregando').fadeOut('slow');
 
         $("#tabela").append("<table border=\"1\"></table>");
 
@@ -30,5 +58,4 @@ $(function() {
             }, "json");  
         }     
     });
-
 });
