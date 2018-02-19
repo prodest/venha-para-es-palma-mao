4d54c313d6a437d7121022f4b522476f230b37b5
David Moura <mouradavid8@gmail.com>
2018-02-19 00:12:11 -0300

    melhorando formupload

--- includes/upload.php
+++ includes/upload.php
@@ -5,7 +5,7 @@
   $target_file = $target_dir . basename($_FILES["arquivo"]["name"]);
 
   if (move_uploaded_file($_FILES["arquivo"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " foi carregado com sucesso!.";
+        echo "O arquivo ". basename( $_FILES["fileToUpload"]["name"]). " foi carregado com sucesso!.";
   } else {
         echo "Desculpe, ocorreu um erro ao carregar o arquivo.";
   }
