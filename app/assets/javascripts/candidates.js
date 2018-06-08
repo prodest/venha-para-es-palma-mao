1fce83694797393be3ea59a0a26850787d6a8764
Dérick Hogan Pimenta <derickhoganpimenta@yahoo.com.br>
2018-06-08 12:58:44 -0300

    First commit to HEROKU

--- app/assets/javascripts/candidates.js
+++ app/assets/javascripts/candidates.js
@@ -1,6 +1,5 @@
 // --- Select2
-//= require select2/dist/js/select2
-
+// require select2/dist/js/select2
 var searchBox = document.getElementById("search-box");
 if (searchBox)
 $("#search-box").select2({
