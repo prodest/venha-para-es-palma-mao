4de85caf4fd6a13f16c45e5c0b6cf1761ee07e25
Gary M. Silva <garymsilva@outlook.com>
2018-06-16 23:45:35 -0300

    Create class Concurso

---
+++ src/modules/concursos/concurso.js
@@ -0,0 +1,11 @@
+class Concurso {
+  constructor(orgao, edital, codigo, vagas, id) {
+    this.id = id;
+    this.orgao = orgao;
+    this.edital = edital;
+    this.codigo = codigo;
+    this.vagas = vagas;
+  }
+}
+
+export default Concurso;
