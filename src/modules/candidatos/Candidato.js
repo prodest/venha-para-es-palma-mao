09e0544ea55c8b37750a78e3674d1378af716b2b
Gary M. Silva <garymsilva@outlook.com>
2018-06-16 22:58:51 -0300

    Create class Candidato

---
+++ src/modules/candidatos/Candidato.js
@@ -0,0 +1,10 @@
+class Candidato {
+  constructor(nome, nascimento, cpf, profissoes) {
+    this.nome = nome;
+    this.nascimento = nascimento;
+    this.cpf = cpf;
+    this.profissoes = profissoes;
+  }
+}
+
+export default Candidato;
