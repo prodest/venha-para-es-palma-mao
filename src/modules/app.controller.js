229d220291c3b6b896be1e0c32d9a549805ff8da
Gary M. Silva <garymsilva@outlook.com>
2018-06-17 01:28:58 -0300

    Create app main controller

---
+++ src/modules/app.controller.js
@@ -0,0 +1,18 @@
+import express from "express";
+import AppService from "./appService";
+
+class AppController {
+  constructor() {
+    this.router = express.Router();
+
+    this.router.get("/candidatos/:cod_concurso", (req, res) => {
+      res.json(AppService.listarCandidatos(req.params.cod_concurso));
+    });
+
+    this.router.get("/concursos/:cpf_candidato", (req, res) => {
+      res.json(AppService.listarConcursos(req.params.cpf_candidato));
+    });
+  }
+}
+
+export default AppController;
